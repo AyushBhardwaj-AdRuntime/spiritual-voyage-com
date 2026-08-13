c0e254edb80656e63357d6810cae559cc6074a48
Ayush <ayushbhardwaj1334@gmail.com>
2026-08-13 21:22:10 +0545

    configure nitro for tanstack start

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { nitro } from 'nitro/vite';
 import viteReact from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
@@ -10,6 +11,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+      nitro(),
     viteReact(),
     tailwindcss(),
     tsconfigPaths(),
